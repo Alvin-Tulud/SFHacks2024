3f9096fb68a0196c5c9bd123f5a96550e58ee1f1
Alvin-Tulud <alvintulud@gmail.com>
2024-04-06 13:12:35 -0700

    map not working

--- locationTest.js
+++ locationTest.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
         
-        initMap(latitude, longitude);
         sendLocation(latitude, longitude);
     }
 
@@ -33,30 +32,3 @@ $(document).ready(function() {
 
     $('.find-state').click(findMyState);
 });
-
-//thanks google
-// Initialize and add the map
-let map;
-
-async function initMap(latitude, longitude) {
-  // The location of Uluru
-  const position = { lat: latitude, lng: longitude };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("userLocation"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Your Location",
-  });
-}
