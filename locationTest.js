f06679d49fc8f599832aec50056fd589bf58e7df
Alvin-Tulud <alvintulud@gmail.com>
2024-04-06 12:11:29 -0700

    sending js to html

--- locationTest.js
+++ locationTest.js
@@ -8,10 +8,9 @@ $(document).ready(function() {
         console.log(position);
         const latitude = position.coords.latitude;
         const longitude = position.coords.longitude;
-
-        document.getElementById("map").src = "https://maps.google.com/maps?center=" + latitude + "," + longitude + "&output=embed";
-
-        sendLocation();
+        
+        initMap(latitude, longitude);
+        sendLocation(latitude, longitude);
     }
 
         const error = () => {
@@ -34,3 +33,32 @@ $(document).ready(function() {
 
     $('.find-state').click(findMyState);
 });
+
+//thanks google
+// Initialize and add the map
+let map;
+
+async function initMap(latitude, longitude) {
+  // The location of Uluru
+  const position = { lat: latitude, lng: longitude };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("userLocation"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Your Location",
+  });
+}
+
+initMap();
